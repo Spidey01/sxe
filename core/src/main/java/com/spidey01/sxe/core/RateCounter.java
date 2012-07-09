438da38efa10c9da49f56c6370ce2a14a4f186b2
Terry M. Poulin <BigBoss1964@gmail.com>
2012-07-09 02:31:46 +0000

    Add a simple class to help count ticks/frames per sec

---
+++ core/src/main/java/com/spidey01/sxe/core/RateCounter.java
@@ -0,0 +1,47 @@
+package com.spidey01.sxe.core;
+
+public class RateCounter {
+    private long mPerSecond;
+    private long ms;
+
+    private String mName;
+    private boolean mDebuggingEnabled;
+
+    public RateCounter(String name, boolean debug) {
+        mName = name;
+        mDebuggingEnabled = debug;
+        ms = System.currentTimeMillis();
+    }
+
+    public RateCounter(String name) {
+        this(name, true);
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void disableDebugging() {
+        mDebuggingEnabled = false;
+    }
+    public void enableDebugging() {
+        mDebuggingEnabled = true;
+    }
+    public boolean isDebuggingEnabled() {
+        return mDebuggingEnabled;
+    }
+
+    public void update() {
+        long now = System.currentTimeMillis();
+
+        if (now > (ms+1000)) {
+            if (isDebuggingEnabled()) {
+                System.out.println(getName()+": "+(mPerSecond-1)+" per second");
+            }
+            ms = now;
+            mPerSecond = 0;
+        }
+        mPerSecond++;
+    }
+
+}
