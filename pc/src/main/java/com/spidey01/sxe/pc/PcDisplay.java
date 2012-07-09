bd8461e27b4b5ca26b1812b331a8eaccc699c24a
Terry M. Poulin <BigBoss1964@gmail.com>
2012-07-09 04:03:16 +0000

    Pcdisplay now has more control over resolution; coutns FPS

--- pc/src/main/java/com/spidey01/sxe/pc/PcDisplay.java
+++ pc/src/main/java/com/spidey01/sxe/pc/PcDisplay.java
@@ -1,17 +1,43 @@
 package com.spidey01.sxe.pc;
 
+import com.spidey01.sxe.core.RateCounter;
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
+import java.util.Arrays;
+
 public class PcDisplay implements com.spidey01.sxe.core.Display {
-    public PcDisplay() {
+
+    private RateCounter mFrameCounter = new RateCounter("Frames");
+
+    public PcDisplay(String desired) {
+        // default to VGA if desired not found
+        DisplayMode wanted = new DisplayMode(640, 480);
+
         try {
-            Display.setDisplayMode(new DisplayMode(640, 480));
+            DisplayMode[] modes = Display.getAvailableDisplayModes();
+
+            for (int i=0; i < modes.length; i++) {
+                DisplayMode c = modes[i];
+                if (c.isFullscreenCapable() && c.toString().startsWith(desired)) {
+                    wanted = c;
+                    break;
+                }
+            }
+
+            Display.setDisplayMode(wanted);
+
         } catch (LWJGLException e) {
             System.out.println("PcDisplay.PcDisplay() couldn't set display mode for LWJGL!");
             e.printStackTrace();
         }
+ 
+    }
+
+    public PcDisplay() {
+        this("640 x 480 x 16 @60");
     }
 
     public boolean create() {
@@ -30,6 +56,7 @@ public class PcDisplay implements com.spidey01.sxe.core.Display {
     }
     public void update() {
             Display.update();
+            mFrameCounter.update();
     }
 
     public boolean isCloseRequested() {
