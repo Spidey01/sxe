5c84130f5d100851ddfb52aa5bb6d0c665d2d01d
Terry M. Poulin <BigBoss1964@gmail.com>
2012-07-27 21:09:01 +0000

    Simple triangle test stuffed into PcDisplay.

--- pc/src/main/java/com/spidey01/sxe/pc/PcDisplay.java
+++ pc/src/main/java/com/spidey01/sxe/pc/PcDisplay.java
@@ -58,6 +58,7 @@ public class PcDisplay implements com.spidey01.sxe.core.Display {
     public boolean create() {
         try {
             Display.create();
+setupTriangle();
         } catch (LWJGLException e) {
             Log.e(TAG, "create() can't create LWJGL display :'(");
             e.printStackTrace();
@@ -72,6 +73,7 @@ public class PcDisplay implements com.spidey01.sxe.core.Display {
 		Display.destroy();
     }
     public void update() {
+        draw_triangle();
         Display.update();
         mFrameCounter.update();
     }
@@ -141,6 +143,94 @@ public class PcDisplay implements com.spidey01.sxe.core.Display {
                                                                : (ctx.OpenGL12 ? "1.2"
                                                                    : (ctx.OpenGL11 ? "1.1"
                                                                         : "wtf"))))))))))))));
+    }
+
+///////////////////////////////////////////////////////////////////////////////
+// Testing stuff
+///////////////////////////////////////////////////////////////////////////////
+
+/* triangle.vert:
+attribute vec2 coord2d;
+void main(void) {
+  gl_Position = vec4(coord2d, 0.0, 1.0);
+}
+ */
+/* triangle.frag:
+
+// shader for the Triangle rendering test code in SnakeGame
+void main(void) {
+  gl_FragColor[0] = 0.0;
+  gl_FragColor[1] = 1.0;
+  gl_FragColor[2] = 0.0;
+}
+ */
+
+    // simple 2D triangle
+    private int mCoord2d = -1;
+    private LwjglGlslProgram mProgram;
+    private static IntBuffer mVBO;;
+    private void setupTriangle() {
+        // Setup shaders.
+        LwjglGlslShader vert = null;
+        LwjglGlslShader frag = null;
+        try {
+            LwjglGlslShaderFactory factory = new LwjglGlslShaderFactory();
+            vert = (LwjglGlslShader)C.getResources().load("shaders/triangle.vert", factory).getObject();
+            frag = (LwjglGlslShader)C.getResources().load("shaders/triangle.frag", factory).getObject();
+        } catch(Exception fml) {
+            Log.wtf(TAG, "Failed loading shaders", fml);
+            fml.printStackTrace();
+        }
+
+        // Setup shader program.
+        mProgram = new LwjglGlslProgram();
+        mProgram.addShader(vert);
+        mProgram.addShader(frag);
+        if (!mProgram.link() || !mProgram.validate()) {
+            Log.wtf(TAG, "Shader failure: "+mProgram.getInfoLog());
+        }
+
+        // Setup vertex data to feed into a VBO.
+        float[] triangleVertices = {
+            0.0f,  0.8f,
+            -0.8f, -0.8f,
+            0.8f,  -0.8f,
+        };
+        FloatBuffer buffer = BufferUtils.createFloatBuffer(triangleVertices.length);
+        buffer.put(triangleVertices);
+        buffer.flip();
+
+        // Generate an ID for our VBO in the video memory and bind it.
+        mVBO = BufferUtils.createIntBuffer(1);
+        GL15.glGenBuffers(mVBO);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, mVBO.get(0));
+
+        // Buffer it to the GPU.
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
+
+        // Create coord2d attribute for our fragment shader.
+        mCoord2d = GL20.glGetAttribLocation(mProgram.getProgram(), "coord2d");
+        if (mCoord2d == -1) {
+            Log.wtf(TAG, "Couldn't bind coord2d attribute!");
+        }
+    }
+    private void draw_triangle() {
+        // Clear the background.
+        GL11.glClearColor(0.5f, 0.0f, 0.5f, 1.0f);
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+
+        // Ready the shader program.
+        GL20.glUseProgram(mProgram.getProgram());
+
+        // Lock and load the coord2d attribute for our fragment shader.
+        GL20.glEnableVertexAttribArray(mCoord2d);
+        GL20.glVertexAttribPointer(mCoord2d, 2, GL11.GL_FLOAT, false, 0, 0);
+ 
+        // feed it to our shader to draw.
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, mVBO.get(0));
+        GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, 3);
+
+        GL20.glDisableVertexAttribArray(mCoord2d);
     }
 }
 
