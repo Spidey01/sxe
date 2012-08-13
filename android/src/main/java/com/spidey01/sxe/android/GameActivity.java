9ef03039fdeb7e0b369a7282a420656f4807b212
Terry M. Poulin <BigBoss1964@gmail.com>
2012-08-13 16:30:46 +0000

    Fix missing super.onDestroy() call in GameActivity.

--- android/src/main/java/com/spidey01/sxe/android/GameActivity.java
+++ android/src/main/java/com/spidey01/sxe/android/GameActivity.java
@@ -25,6 +25,7 @@ package com.spidey01.sxe.android;
 
 import com.spidey01.sxe.core.Display;
 import com.spidey01.sxe.core.GameEngine;
+import com.spidey01.sxe.core.Log;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -32,15 +33,17 @@ import android.os.Bundle;
 public abstract class GameActivity extends Activity {
     private static final String TAG = "GameActivity";
 
+    public abstract GameEngine getGameEngine();
+
     /** Called when the activity is first created. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
-    public abstract GameEngine getGameEngine();
-
     protected void onDestroy() {
+        super.onDestroy();
+
         // not sure if this will really make a difference yet.
         getGameEngine().stop();
     }
