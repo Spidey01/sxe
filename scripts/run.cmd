d1145d072d51b7e5ce0f0a2bd4817f5006023daa
Terry M. Poulin <BigBoss1964@gmail.com>
2013-03-24 15:30:07 -0400

    Rig run.cmd to use set XDG_CONFIG_HOME to .\tmp

--- scripts/run.cmd
+++ scripts/run.cmd
@@ -4,6 +4,6 @@ IF "%1" == "" (
 	goto :eof
 )
 
-IF "%XDG_CONFIG_HOME%" == "" (SET XDG_CONFIG_HOME=.)
+IF "%XDG_CONFIG_HOME%" == "" (SET XDG_CONFIG_HOME=.\tmp)
 
 demos\%1\pc\build\install\%1-pc\bin\%1-pc.bat %2 %3 %4 %5 %6 %7 %8 %9
