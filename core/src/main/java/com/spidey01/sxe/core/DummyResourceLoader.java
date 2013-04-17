05373156dbdc08d982d6582895716b6780d0d365
Terry M. Poulin <BigBoss1964@gmail.com>
2013-04-17 16:53:44 +0000

    Add DummyResourceLoader missing from earlier commit.

---
+++ core/src/main/java/com/spidey01/sxe/core/DummyResourceLoader.java
@@ -0,0 +1,55 @@
+/*-
+ * Copyright (c) 2013-current, Terry Mathew Poulin <BigBoss1964@gmail.com>
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *	1. The origin of this software must not be misrepresented; you must
+ *	   not claim that you wrote the original software. If you use this
+ *	   software in a product, an acknowledgment in the product
+ *	   documentation would be appreciated but is not required.
+ *
+ *	2. Altered source versions must be plainly marked as such, and must
+ *	   not be misrepresented as being the original software.
+ *
+ *	3. This notice may not be removed or altered from any source
+ *	   distribution.
+ */
+
+package com.spidey01.sxe.core;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/** Dummy ResourceLoader that returns the bitbucket. */
+public class DummyResourceLoader  implements ResourceLoader {
+
+    public InputStream getInputStream(File path)
+        throws IOException
+    {
+        return getInputStream(path.getPath());
+    }
+
+    public InputStream getInputStream(String path)
+        throws IOException
+    {
+        try { // unix
+            return new FileInputStream("/dev/null");
+        } catch (FileNotFoundException e1) {
+            try { // dos
+                return new FileInputStream("nul:");
+            } catch (FileNotFoundException e2) {
+                throw new IOException(DummyResourceLoader.class + ": Unsupported OS.");
+            }
+        }
+    }
+}
+
