e82e7e41d5d8d69f9b96680c391bbe0fe2893d71
Terry M. Poulin <BigBoss1964@gmail.com>
2013-06-28 18:33:34 -0400

    Fix run.cmd because cmd.exe is gay.

--- run.cmd
+++ run.cmd
@@ -6,6 +6,6 @@ IF "%1" == "" (
 
 IF "%XDG_CONFIG_HOME%" == "" (SET XDG_CONFIG_HOME=.\tmp)
 
-.\gradlew.bat ":demos:%1:pc:installApp"
-IF ERRORLEVEL 0 GOTO :eof
+call .\gradlew.bat --daemon ":demos:%1:pc:installApp"
+IF ERRORLEVEL 1 GOTO :eof
 demos\%1\pc\build\install\%1-pc\bin\%1-pc.bat %2 %3 %4 %5 %6 %7 %8 %9
