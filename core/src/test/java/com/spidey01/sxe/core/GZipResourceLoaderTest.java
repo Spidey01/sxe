c6fb9e08255d283862424faf7649563af95c48b4
Terry M. Poulin <BigBoss1964@gmail.com>
2013-07-07 17:36:03 -0400

    :core add support for loading from GZip'd files.

---
+++ core/src/test/java/com/spidey01/sxe/core/GZipResourceLoaderTest.java
@@ -0,0 +1,55 @@
+/*-
+ * Copyright (c) 2013-current, Terry Mathew Poulin <BigBoss1964@gmail.com>
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *	1. The origin of this software must not be misrepresented; you must
+ *	   not claim that you wrote the original software. If you use this
+ *	   software in a product, an acknowledgment in the product
+ *	   documentation would be appreciated but is not required.
+ *
+ *	2. Altered source versions must be plainly marked as such, and must
+ *	   not be misrepresented as being the original software.
+ *
+ *	3. This notice may not be removed or altered from any source
+ *	   distribution.
+ */
+
+package com.spidey01.sxe.core;
+
+import org.junit.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.*;
+
+public class GZipResourceLoaderTest extends UnitTest {
+    private static GZipResourceLoader sLoader = new GZipResourceLoader();
+
+    @BeforeClass
+    public static void setup() {
+        UnitTest.setup();
+    }
+
+
+    @Test
+    public void simplePathTest() throws IOException {
+        String from = "src/test/resources";
+        String what = "PathResourceLoader.txt.gz";
+        File txt = new File(from, what);
+        Assume.assumeTrue(txt.exists());
+
+
+        String expected = "Test dummy for PathResourceLoader.\n";
+        Assert.assertEquals("Loading a gzip compressed text file.", expected,
+                            Utils.slurp(sLoader.getInputStream(from, what)));
+    }
+
+}
+
