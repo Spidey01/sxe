3acb89247f98e9cea6a08658d41e630ff764ff5d
Terry M. Poulin <BigBoss1964@gmail.com>
2013-07-31 19:16:56 +0000

    Basic :scripting:jruby support.
    Useful links:
    
    	- https://github.com/jruby/jruby/wiki/Integration-With-JVM-Languages
    	- https://github.com/jruby/jruby/wiki/RedBridge
    	- https://github.com/jruby/jruby/wiki/CallingJavaFromJRuby
    	- https://github.com/jruby/jruby/wiki
    	- http://jruby.org/apidocs/org/jruby/embed/ScriptingContainer.html

---
+++ scripting/testhelpers/src/main/java/com/spidey01/sxe/scripting/testhelpers/JavaToRuby.java
@@ -0,0 +1,79 @@
+/*-
+ * Copyright (c) 2013-current, Terry Mathew Poulin <BigBoss1964@gmail.com>
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *	1. The origin of this software must not be misrepresented; you must
+ *	   not claim that you wrote the original software. If you use this
+ *	   software in a product, an acknowledgment in the product
+ *	   documentation would be appreciated but is not required.
+ *
+ *	2. Altered source versions must be plainly marked as such, and must
+ *	   not be misrepresented as being the original software.
+ *
+ *	3. This notice may not be removed or altered from any source
+ *	   distribution.
+ */
+
+package com.spidey01.sxe.scripting.testhelpers;
+
+import org.junit.*;
+
+import com.spidey01.sxe.core.Log;
+
+import com.spidey01.sxe.scripting.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class JavaToRuby {
+    private static final String TAG = "JavaToRuby";
+    private static final String sResourceProperty = "scripting.testhelpers.resources";
+
+
+    public static void simple(ScriptManager manager) throws IOException, FileNotFoundException {
+        String resourcesDir = System.getProperty(sResourceProperty);
+        if (resourcesDir == null) {
+            Log.d(TAG, "Please set "+sResourceProperty+" property in build.gradle.");
+        }
+
+        File source = new File(resourcesDir, "JavaWrapper.rb");
+
+        manager.eval(manager.createScript(), source);
+    }
+
+
+    public static void get(ScriptManager manager) {
+        Script script = manager.createScript();
+        /*
+         * JRuby gives a Long and Double for simple whole and real numbers.
+         */
+        long x = (Long)manager.eval(script, "x = 7");
+        Assert.assertEquals(7, x);
+    }
+
+
+    public static void put(ScriptManager manager) {
+        Script script = manager.createScript();
+        JavaClass javaClass = new JavaClass();
+
+        manager.put(script, "javaClass", javaClass);
+        Assert.assertTrue((Boolean)manager.eval(script, "javaClass.returnsTrue()"));
+
+        // since javaClass is a Java method, it should be returned as a Long.
+        long sum = (Long)manager.eval(script, "javaClass.returnsSum(2, 2)");
+        Assert.assertEquals(4, sum);
+
+        String foobar = String.valueOf(manager.eval(script, "javaClass.returnsStrCat('foo', 'bar')"));
+        Assert.assertEquals("foobar", foobar);
+    }
+}
+
+
