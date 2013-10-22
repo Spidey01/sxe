a456166c3b24b8837f91a999e8536baa2329c559
Terry M. Poulin <BigBoss1964@gmail.com>
2013-10-22 19:28:31 +0000

    Fix run.sh to set java.library.path correctly.

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ ! -f "$0" ]; then
+    "Can't find $0. Wrong directory?"
+    exit 127
+fi
+croot=`dirname $0`
+
 if [ $# -eq 0 ]; then
     echo "Select a demo, e.g. $0 helloworld"
     exit 1
@@ -11,5 +17,12 @@ shift
 XDG_CONFIG_HOME="`pwd`/tmp"
 export XDG_CONFIG_HOME
 
-./gradlew --daemon ":demos:${demo}:pc:installApp" || exit $?
+# The run scripts expect to be called from it's bin directory and sets
+# java.library.path accordingly. So we need to overrule it here.
+# 
+eval "`echo $demo | awk '{print toupper($0)}'`_PC_OPTS=\"'-Djava.library.path=${croot}/demos/${demo}/pc/build/install/${demo}-pc/lib/natives'\""
+eval "export `echo helloworld | awk '{print toupper($0)}'`_PC_OPTS"
+
+
+$croot/gradlew --daemon ":demos:${demo}:pc:installApp" || exit $?
 "demos/${demo}/pc/build/install/${demo}-pc/bin/${demo}-pc" "$@"
