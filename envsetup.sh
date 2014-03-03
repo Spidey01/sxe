abe111faa3a38874fe81062c26c3724301870b6b
Terry M. Poulin <BigBoss1964@gmail.com>
2014-03-03 19:57:31 +0000

    Upgrade envsetup to 1.1.
    I split out envsetup to it's own git repository at
    https://github.com/Spidey01/envsetup. This imports version 1.1.

--- envsetup.sh
+++ envsetup.sh
@@ -1,3 +1,36 @@
+#-
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# For more information, please refer to <http://unlicense.org>
+
+ENVSETUP_VERSION="1.1"
+
+# database like list of project files, see 'filelist'.
+# path is relative to projet root.
+#
+ENVSETUP_FILELIST=tmp/filelist
+
 
 # in honor of Android.
 hmm() { # This help.
@@ -7,7 +40,7 @@ hmm() { # This help.
     # Supported OS'es include the path here, so >_>.
     # Also this skips funcs with !a-z names (e.g. for internals).
     # It also skips undocumented.
-    cat `gettop`/envsetup.sh | grep '^[a-z]*() {' | \
+    cat `gettop`/envsetup.{sh,local.sh} | grep '^[a-z]*() {' | \
         sed -e 's/() { # /\t/' -e 's/[a-z]*() {.*$//' -e '/^$/d' -e 's/^/\t/' #| sort
 
     echo
@@ -16,20 +49,33 @@ hmm() { # This help.
 
 
 guess_is_top() {
+    # These files are always present at top.
+    if [ -f "envsetup.cmd" -a -f "envsetup.sh" ]; then return 0; fi
+    if ! [ -f "COPYING" -a -f "README" ]; then return 127; fi
+
+    # are we a gradle top?
     [ \
         -f "gradlew" \
         -a -f "build.gradle" \
         -a -f "settings.gradle" \
         -a -f "gradle/wrapper/gradle-wrapper.properties" \
-        -a -d "core" \
-        -a -d "android" \
-        -a -d "pc" \
-    ]
+    ] && return 0
+
+    # are we a make top?
+    [ \
+        -f "Makefile" \
+        -a -d "src" -a -d "include" \
+    ] && return 0
 }
 
 
 guess_is_project() {
-    [ -f "build.gradle" ]
+    [ -f "Makefile" -o \
+      -f "build.gradle" -o \
+      -f "build.xml" -o \
+      -f "CMakeLists" -o \
+      -f "premake.lua" \
+    ]
 }
 
 
@@ -66,32 +112,78 @@ mpushd() { # call pushd with a module name.
 
 
 lsproj() { #
-    local here dir parent targets task tasks
-    for dir in `find ${@:-.} -name .git -prune -o -type f -name build.gradle`; do
-        # echo debug dir=$dir dirname dir=`dirname $dir`
-        here="`dirname $dir | sed -e 's/build.gradle//' -e 's/^\.\///' -e 's/\//:/g'`"
-        # echo "here: $here"
+    local here dir parent project_file targets task tasks type
+    for project_file in $(find ${@:-.} -name .git -prune \
+        -o -type f -name build.gradle \
+        -o -type f -name Makefile \
+        -o -type f -name Makefile.am \
+        )
+    do
+        dir="$(dirname "$project_file")"
+        type="$(basename "$project_file")"
+        # echo "debug dir=$dir project_file=$project_file type=$type"
+
         # skip .
         [ "$here" = '.' ] && continue
+        
+        case "$type" in
+            build.gradle)
+                # gradle uses a :module:name notaton. Modules come from settings.gradle.
 
-        parent="$(pwd | sed -e 's:'"$(gettop)"'::' -e 's/\//:/g')"
-        # echo "parent: $parent"
+                # our module name.
+                here="`echo $dir | sed -e 's/^\.\///' -e 's/\//:/g'`"
+                # echo "here=$here"
 
-        # skip if not in settings.gradle file.
-        grep include "$(gettop)/settings.gradle" | grep -q "$parent:$here" || continue
-        echo "$parent:$here"
+                # figure out what the parents of this project is.
+                parent="$(pwd | sed -e 's:'"$(gettop)"'::' -e 's/\//:/g')"
+                # echo "parent: $parent"
+
+                # skip if not in settings.gradle file.
+                grep include "$(gettop)/settings.gradle" | grep -q "$parent:$here" && echo "$parent:$here"
+                ;;
+            Makefile|Makefile.*)
+                # assume it works this way.
+                echo "$dir" | sed -e 's/^\.\///'
+                ;;
+            *)
+                # not a project.
+                ;;
+        esac
+
     done
 }
 
 
-m() { # Makes from the top of the tree.
-    cpushd > /dev/null
-    [ -f "$(gettop)/tmp/.m-clears-screen" ] && clear
-
+_maybe_use_script() { ## run it's args and log to tmp/build.log
     if [ -f "$(gettop)/tmp/.m-uses-script" ] && type script >/dev/null; then
-        script -c "\"$(gettop)/gradlew\" --daemon \"${@:-build}\"" "$(gettop)/tmp/gradlew.log"
+        script -c "$*" "$(gettop)/tmp/build.log"
     else
-        "$(gettop)/gradlew" --daemon "${@:-build}"
+        eval $*
+    fi
+}
+
+
+m_gradle() { # runs gradle based build in cwd.
+    _maybe_use_script "$(gettop)/gradlew" --daemon "${@:-build}"
+}
+
+
+m_make() { # runs make based build in cwd.
+    _maybe_use_script make $*
+}
+
+
+m() { # Makes from the top of the tree. Selects m_tool correctly.
+    cpushd > /dev/null
+    if guess_is_project; then
+        [ -f "$(gettop)/tmp/.m-clears-screen" ] && clear
+        if [ -f Makefile ]; then
+            m_make $*
+        elif [ -f build.gradle ]; then
+            m_gradle $*
+        else
+            echo "Don't know how to build this kind of project."
+        fi
     fi
     popd
 }
@@ -133,55 +225,25 @@ m() { # Makes from the top of the tree.
 # }
 
 
-mma() { # Builds all of the modules in the current directory, and their dependencies.
+mma() { # Run m for all of the modules in the current directory.
     local task project targets
 
     for project in `lsproj`; do
-        for task in ${@:-build}; do
-            targets="$targets $project:$task"
-        done
+        if echo "$project" | grep -q ':'
+        then # gradle project.
+            for task in ${@:-build}; do
+                targets="$targets $project:$task"
+            done
+        elif [ -f "${project}/Makefile" ]; then
+            targets="$targets $project-$1"
+        fi
     done
-    m $targets
-
+    echo m $targets
 }
 
 
 # mmma() { # Builds all of the modules in the supplied directories, and their dependencies.
 # }
-
-
-rd() { # :run a demo by name
-    m ":demos:${1}:pc:run"
-    # TODO: pass args to app as a property?
-}
-
-
-rdemo() { # alias for rd
-    rd $*
-}
-
-
-idemo() { # :installApp a demo by name
-    m ":demos:${1}:pc:installApp"
-}
-
-
-irdemo() { # installdemo and execute a demo by name with following args.
-    local demo
-    demo=$1
-    shift
-    idemo $demo
-    eval \
-        env \
-            XDG_DATA_DIRS="`gettop`/demos/$demo/pc/src/dist/share" \
-            XDG_CONFIG_DIRS="`gettop`/demos/$demo/pc/src/dist/etc" \
-            XDG_DATA_HOME="`gettop`/tmp/share" \
-            XDG_CONFIG_HOME="`gettop`/tmp/config" \
-            XDG_CACHE_HOME="`gettop`/tmp/cache" \
-            "`echo $demo | awk '{print toupper($0)}'`_PC_OPTS=\"'-Djava.library.path=$(gettop)/demos/${demo}/pc/build/install/${demo}-pc/lib/natives'\"" \
-            \
-                "$(gettop)/demos/${demo}/pc/build/install/${demo}-pc/bin/${demo}-pc" "$@"
-}
 
 
 jgrep() { # runs grep on all local Java source files.
@@ -210,11 +272,83 @@ mangrep() { #  runs grep on all local AndroidManifest.xml files.
 }
 
 
-choosejdk() {
-    local option
+filelist() { # create index of project files.
+    local project dirs p
+
+    croot
+    echo -n "Creating index..."
+
+
+    dirs=""
+    for project in $(lsproj); do
+        # make sure any gradle projects are converted to paths.
+        # first -e is for the leading :!
+        p="$(echo "./$project" | sed -e 's/://' -e 's/:/\//g')"
+        # echo "p=$p"
+        dirs="$dirs $p"
+    done
+
+    # exclude what looks like a temporary or hidden directory.
+    find $dirs -not \( \
+        -type d -name \.\*  -prune -o \
+        -type d -name tmp   -prune -o \
+        -type d -name out   -prune -o \
+        -type d -name build -prune -o \
+        -type d -name obj   -prune \
+    \) -type f | sort | uniq > "./$ENVSETUP_FILELIST"
+
+
+    echo " Done"
+    cd "$OLDPWD"
+}
+
+
+flgrep() { # grep against the filelist.
+    grep "$@" "$(gettop)/$ENVSETUP_FILELIST"
+}
+
+
+godir() { # Go to the directory containing a file.
+    local T db matches count
+
+    T=$(gettop)
+    db="$T/${ENVSETUP_FILELIST}"
+
+    if [ ! -f "$db" ]; then
+        filelist
+    fi
+
+    matches=($(grep $* "$db"))
+    count=${#matches[@]}
+
+    [ "$count" -eq 0 ] && return # not found
+    [ "$count" -eq 1 ] && {
+        mpushd "$(dirname "${matches[0]}")"
+        return
+    }
+
+    echo "count $count"
+    select which in "${matches[@]}"; do
+        mpushd "$(dirname "$which")"
+        return
+    done
+}
+
+
+choosejdk() { # Export a JAVA_HOME.
+    local option jdk where
+
+    JDK_LIST=""
+
+    for where in /usr/lib/jvm /usr/local/lib/jvm; do
+        [ -d "$where" ] || continue
+        for jdk in $(ls "$where"); do
+            JDK_LIST="$JDK_LIST ${where}/${jdk}"
+        done
+    done
 
     echo "Select a JDK:"
-    select option in `ls /usr/lib/jvm`; do
+    select option in $JDK_LIST; do
         export JAVA_HOME="$option"
         break
     done
@@ -246,7 +380,7 @@ check_android() { # fuzzy helper for ANDROID_HOME.
 }
 
 
-_sxe_complete_projects() { ## bash completion function for project names.
+_envsetup_complete_projects() { ## bash completion function for project names.
     local cur prev opts
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
@@ -263,27 +397,22 @@ _sxe_complete_projects() { ## bash completion function for project names.
 }
 
 
-_sxe_complete_demos() { ## bash completion function for demo names.
-    local cur prev opts
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="$(ls $(gettop)/demos | sed -e 's/\///')"
-
-    COMPREPLY=($(compgen -W "$opts" -- ${cur}))  
-
-    return 0
-
-}
-
-
 # complete project names for these commands.
-complete -o nospace -F _sxe_complete_projects gradlew ./gradlew m mma
-
-# complete demo names for these commands.
-complete -F _sxe_complete_demos rd rdemo idemo irdemo
+complete -o nospace -F _envsetup_complete_projects m mma m_make m_gradle
 
 check_android
 
-[ -f "$(gettop)/envsetup.local.sh" ] && . "$(gettop)/envsetup.local.sh"
+if [ -f "$(gettop)/envsetup.local.sh" ]; then
+    . "$(gettop)/envsetup.local.sh"
+else
+    cat << EOF > "$(gettop)/envsetup.local.sh"
+#
+# Place local changes to envsetup.sh here.
+# This includes environment variables, etc.
+#
+
+PROJECT_NAME="$(basename $(pwd))"
+
+EOF
+fi
 
