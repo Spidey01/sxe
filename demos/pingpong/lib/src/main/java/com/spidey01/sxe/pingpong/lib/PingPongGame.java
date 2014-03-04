738c42fb491c5e2014b5f604e1a55b3583ba775a
Terry M. Poulin <BigBoss1964@gmail.com>
2014-03-03 21:48:29 -0500

    Flesh out PingPongGame
    Display a Text until 'S' is pressed to start the game.
    
    Bound W/S, A/D to up/down when the game is running.

--- demos/pingpong/lib/src/main/java/com/spidey01/sxe/pingpong/lib/PingPongGame.java
+++ demos/pingpong/lib/src/main/java/com/spidey01/sxe/pingpong/lib/PingPongGame.java
@@ -26,27 +26,47 @@ package com.spidey01.sxe.pingpong.lib;
 import com.spidey01.sxe.core.Game;
 import com.spidey01.sxe.core.GameEngine;
 import com.spidey01.sxe.core.Log;
+import com.spidey01.sxe.core.Text;
+import com.spidey01.sxe.core.input.InputManager;
+import com.spidey01.sxe.core.input.KeyEvent;
+import com.spidey01.sxe.core.input.KeyListener;
 
 /** A simple game demo inspired by Pong.
  */
 public class PingPongGame
     extends Game
+    implements KeyListener
 {
     private static final String TAG = "PingPongGame";
+
+    private static final String START_MESSAGE = "Press S key or tap to start game.";
+    private static final Text sStartMessage = new Text(START_MESSAGE);
+
 
     @Override
     public String getName() {
         return TAG;
     }
 
+
     @Override
     public boolean start(GameEngine engine) {
         super.start(engine);
+        mState = State.STARTING;
 
         Log.v(TAG, "Ping Pong Game is starting.");
 
+        InputManager im = mGameEngine.getInputManager();
+        im.addKeyListener("Q", this); /* quit key */
+        im.addKeyListener("S", this); /* start and movement key */
+        im.addKeyListener("W", this); /* movement key */
+        im.addKeyListener("A", this); /* movement key */
+        im.addKeyListener("D", this); /* movement key */
+        mGameEngine.getDisplay().addFrameStartedListener(sStartMessage);
+
         return true;
     }
+
 
     @Override
     public void stop() {
@@ -58,8 +78,10 @@ public class PingPongGame
         Log.v(TAG, "Ping Pong Game is stopping.");
     }
 
+
     @Override
     public void tick() {
+        Log.xtrace(TAG, "mState =", mState);
         switch (mState) {
             case STARTING: {
             } break;
@@ -68,6 +90,42 @@ public class PingPongGame
             case STOPPING: {
             } break;
         }
+    }
+
+
+    @Override
+    public boolean onKey(KeyEvent event) {
+        /* Quit game. */
+        if (event.getKeyName().equals("Q")) {
+            requestStop();
+            return true;
+        }
+
+        /* Start game. */
+        if (mState == State.STARTING && event.getKeyName().equals("S")) {
+            mGameEngine.getDisplay().removeFrameStartedListener(sStartMessage);
+            mState = State.RUNNING;
+            return true;
+        }
+
+        /* Don't bind keys until started. */
+        if (mState != State.RUNNING) {
+            return false;
+        }
+
+        /* Move UP. */
+        if (event.getKeyName().equals("W") || event.getKeyName().equals("A")) {
+            Log.xtrace(TAG, "Move up");
+            return true;
+        }
+
+        /* Move DOWN. */
+        if (event.getKeyName().equals("S") || event.getKeyName().equals("D")) {
+            Log.xtrace(TAG, "Move down");
+            return true;
+        }
+
+        return false;
     }
 
 }
