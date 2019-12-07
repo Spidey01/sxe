d1fb21828780d255b05feb9fe00d01fa3c38ce0c
Terry M. Poulin <BigBoss1964@gmail.com>
2019-12-07 16:26:11 -0500

    Snapshot current WIP.
    demos is now a subpackage.
    
    Getting NullDemo to compile gave me a half day of finding a effing dllimport/dllexport typo.
    
    NGEN really should add a way to make higher up vars, so I can pass cxxflags from parent -> child project.

--- demos/nulldemo/src/NullDemo.cpp
+++ demos/nulldemo/src/NullDemo.cpp
@@ -2,30 +2,29 @@
 #include "NullDemo.h"
 
 #include <sxe/core/GameEngine.hpp>
+#include <sxe/logging.hpp>
 
 namespace demos {
 
-std::string NullDemo::sTAG = "NullDemo";
+std::string NullDemo::TAG = "NullDemo";
 
 std::string NullDemo::getName() const
 {
-    return sTAG;
+    return TAG;
 }
 
-#if 0
-bool NullDemo::start(sxe::core::GameEngine& engine)
+bool NullDemo::start(sxe::core::GameEngine* engine)
 {
     sxe::core::Game::start(engine);
 
-    #if 0 // java:
-    Log.v(TAG, "Null demo is starting.");
+    Log::v(TAG, "Null demo is starting.");
 
+    #if 0 // java:
     mGameEngine.getInputManager().addKeyListener(InputCode.IC_Q, this);
     #endif
 
     return true;
 }
-#endif
 
 }
 
--- demos/nulldemo/src/NullDemo.h
+++ demos/nulldemo/src/NullDemo.h
@@ -1,6 +1,7 @@
 #ifndef SXE_DEMOS_NULLDEMO_H
 #define SXE_DEMOS_NULLDEMO_H
 
+#include <sxe/api.hpp>
 #include <sxe/core/Game.hpp>
 
 #include <string>
@@ -12,12 +13,11 @@ namespace demos {
 
         std::string getName() const override;
 
-        bool start(sxe::core::GameEngine& engine) override;
+        bool start(sxe::core::GameEngine* engine) override;
 
       private:
-        static std::string sTAG;
+        static std::string TAG;
     };
 }
 
 #endif
-
