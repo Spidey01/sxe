43d7daf6b2793abb5cdc23f93175f0f3beb28f44
Terry M. Poulin <BigBoss1964@gmail.com>
2019-12-07 13:12:00 -0500

    Create a higher level api.hpp.
    Makes sure the omni-present cxxversion/stdheaders are included.
    Makes sure dllexport/dllimport headers are included.
    
    Makes sure SXE_PUBLIC and SXE_PRIVATE are defined to decorate symbols as
    public (exported/imported) or private (hidden).
    
    When building the DLL, make sure that SXE_DLL and SXE_DLL_EXPORTS are
    defined. When compiling code that will link the shared library, make sure
    to define SXE_DLL.
    
    H/T to https://gcc.gnu.org/wiki/Visibility for helping explain how this
    relates to modern GCC, versus just ignoring it off MSVC.

---
+++ core/include/sxe/api.hpp
@@ -0,0 +1,48 @@
+#ifndef SXE_SXEAPI__HPP
+#define SXE_SXEAPI__HPP
+/*-
+ * Copyright (c) 2019-current, Terry Mathew Poulin <BigBoss1964@gmail.com>
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *	1. The origin of this software must not be misrepresented; you must
+ *	   not claim that you wrote the original software. If you use this
+ *	   software in a product, an acknowledgment in the product
+ *	   documentation would be appreciated but is not required.
+ *
+ *	2. Altered source versions must be plainly marked as such, and must
+ *	   not be misrepresented as being the original software.
+ *
+ *	3. This notice may not be removed or altered from any source
+ *	   distribution.
+ */
+
+#include <sxe/cxxversion.hpp>
+#include <sxe/stdheaders.hpp>
+
+#include <sxe/dllexport.hpp>
+#include <sxe/dllimport.hpp>
+
+#if defined(SXE_DLL)
+    /* SxE shared library. */
+    #ifdef SXE_DLL_EXPORTS
+        /* Building the SxE shared library. */
+        #define SXE_PUBLIC SXE_DLL_EXPORT
+    #else
+        /* Linking to SxE shared library. */
+        #define SXE_PUBLIC SXE_DLL_IMPORT
+    #endif // SXE_DLL_EXPORTS
+    #define SXE_PRIVATE SXE_DLL_HIDDEN
+#else
+    /* SxE static library. */
+    #define SXE_PUBLIC
+    #define SXE_PRIVATE
+#endif // SXE_DLL
+
+#endif // SXE_SXEAPI__HPP
