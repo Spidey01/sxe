0ed31b512c7c9fad3b0e9576f8a5a79ccd565297
Terry M. Poulin <BigBoss1964@gmail.com>
2019-12-13 23:11:38 -0500

    Convert Xdg and FileSystem.
    FileSystem is mostly the same, but is a namespace rather than a class of
    statics. Because this isn't Java anymore ;).
    
    Xdg is mostly the same, but is made to be an instance rather than a class
    of static fields. Because the initialization sugar left with the coffee.

---
+++ core/src/sys/FileSystem.cpp
@@ -0,0 +1,76 @@
+/*-
+ * Copyright (c) 2012-current, Terry Mathew Poulin <BigBoss1964@gmail.com>
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *	1. The origin of this software must not be misrepresented; you must
+ *	   not claim that you wrote the original software. If you use this
+ *	   software in a product, an acknowledgment in the product
+ *	   documentation would be appreciated but is not required.
+ *
+ *	2. Altered source versions must be plainly marked as such, and must
+ *	   not be misrepresented as being the original software.
+ *
+ *	3. This notice may not be removed or altered from any source
+ *	   distribution.
+ */
+
+#include "sxe/core/sys/FileSystem.hpp"
+#include "sxe/core/sys/Platform.hpp"
+
+namespace sxe { namespace core { namespace sys { namespace FileSystem {
+
+
+SXE_PUBLIC string getUserDir()
+{
+    static char* sUserDir;
+
+    if (sUserDir == nullptr) {
+        char* e = std::getenv("HOME");
+        if (e == nullptr) {
+            e = std::getenv("USERPROFILE");
+            if (e == nullptr) {
+                /*
+                   throw new
+                   std::runtime_exception("Can't determine users home directory.");
+                   */
+                /*
+                 * Default to empty string because there's no var on Android.
+                 * Ideally we wanna come up with something more suitable.
+                 */
+                return "";
+            }
+        }
+        sUserDir = e;
+    }
+
+    return sUserDir;
+}
+
+// SXE_PUBLIC FILE* getBitBucketFile();
+
+SXE_PUBLIC path getBitBucketPath()
+{
+    /*
+     * SxE 1.x always returned /dev/null.
+     *
+     * But the real value for Windows is NUL. DOS history, go figure.
+     */
+
+    if (Platform().isWindows())
+        return "NUL";
+
+    return "/dev/null";
+}
+
+// SXE_PUBLIC string find(string[] dirs, string name);
+
+
+} } } }
+
