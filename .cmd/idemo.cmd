cea04273897d6b7997317ace9e2317e971a43a17
Terry M. Poulin <BigBoss1964@gmail.com>
2019-12-29 21:01:14 -0500

    idemo.cmd: build all if no demo specified.
    Like the Linux version does.

--- .cmd/idemo.cmd
+++ .cmd/idemo.cmd
@@ -3,11 +3,6 @@ IF NOT DEFINED PROJECT_ROOT (
 	ECHO "PROJECT_ROOT not defined, please setup your environment"
 	GOTO :eof
 )
-
-IF "%1" == "" (
-	echo Select demo, e.g. .\%0 helloworld
-	goto :eof
-)
 @ECHO ON
 
 ngen
