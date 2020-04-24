14ec47ad3ca88560bee6953b376d7d319551e76b
Terry M. Poulin <BigBoss1964@gmail.com>
2020-04-24 16:14:39 -0400

    Add remaining deps to bootstrap.cmd.
    If find_package() in CMakeLists.txt, and vendor\libarchive\CMakeLists.txt
    both can't find zlib when zlib is built using add_subdirectory, it's just
    less freakin' bother to put it in the bootstraper.
    
    Or you know, use something less painful like my ngen >_>.

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -17,10 +17,26 @@ FOR %%d IN (lib include bin share) DO (
 :LOCATE_CPPUNIT
 @REM Best way to build upstream cppunit on NT is to use msbuild. Like our ngen setup script for it.
 CALL "%PROJECT_ROOT%\vendor\cppunit.ngen\cppunit-setup.cmd"
+@IF errorlevel 1 goto :EOF
 
 :LOCATE_BOOST
 @REM Boost is a monster.
 CALL "%PROJECT_ROOT%\vendor\boost.ngen\boost-setup.cmd"
+@IF errorlevel 1 goto :EOF
+
+:LOCATE_ZLIB
+@REM Can't build zlib from CMakeLists.txt, and also find_package() it without pain.
+cmake -S "%PROJECT_ROOT%\vendor\zlib" -B "%PROJECT_TMPDIR%\zlib" -G Ninja "-DCMAKE_INSTALL_PREFIX=%PROJECT_DISTDIR%" -DCMAKE_BUILD_TYPE=Release
+@IF errorlevel 1 goto :EOF
+ninja -C "%PROJECT_TMPDIR%\zlib" install
+@IF errorlevel 1 goto :EOF
+
+:LOCATE_LIBARCHIVE
+cmake -S "%PROJECT_ROOT%\vendor\libarchive" -B "%PROJECT_TMPDIR%\libarchive" -G Ninja "-DCMAKE_INSTALL_PREFIX=%PROJECT_DISTDIR%" -DCMAKE_BUILD_TYPE=Release
+@IF errorlevel 1 goto :EOF
+ninja -C "%PROJECT_TMPDIR%\libarchive" install
+@IF errorlevel 1 goto :EOF
+
 
 ECHO Set your SXE_SDK=%PROJECT_DISTDIR% or get smarter.
 
