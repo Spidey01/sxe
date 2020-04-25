429856fcfc1d7d7971e348aab5441f1b8479fb45
Terry M. Poulin <BigBoss1964@gmail.com>
2020-04-25 12:44:22 -0400

    Make CMAKE_BUILD_TYPE default to Release, or RelWithDebInfo.

---
+++ cmake/Modules/DefaultBuildType.cmake
@@ -0,0 +1,19 @@
+# vim: set filetype=cmake tabstop=4 shiftwidth=4 expandtab :
+# Adapted from https://blog.kitware.com/cmake-and-the-default-build-type/
+
+# Set a default build type if none was specified
+set(default_build_type "Release")
+if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
+  set(default_build_type "RelWithDebInfo")
+endif()
+ 
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+  message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
+  set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE
+      STRING "Choose the type of build." FORCE)
+
+  # Set the possible values of build type for cmake-gui
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
+    "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+endif()
+
