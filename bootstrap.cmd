3e76143159f4d7eb2e55a584ff534d2992c47da4
Terry M. Poulin <BigBoss1964@gmail.com>
2020-05-08 11:13:33 -0400

    Rewrite bootstrap.cmd to use vcpkg.
    This copies vendor\vcpkg -> tmp\vcpkg, calls its bootstrap-vcpkg.bat.
    Then copies it to %PROJECT_ROOT%, and uses it to install the deps.
    
    Likewise, envsetup.project.cmd now sets VCPKG_ROOT accordingly.

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -2,67 +2,44 @@
 
 ECHO Bootstrapping SXE SDK.
 
-
 :GIT_SUBMODULES
 @REM Required because envsetup is a submodule.
 ECHO Updating git submodules.
 git submodule init
 git submodule update
 
-
 :CALL_ENVSETUP
 ECHO Setting up environment.
-CALL envsetup\envsetup.cmd
+IF NOT DEFINED PROJECT_ROOT ( CALL envsetup\envsetup.cmd )
 
-:SETUP_DIST_TREE
-@REM vendor\foo.ngen\foo-setup.cmd expect these to exist 'cuz written for ngen/ninja.
-FOR %%d IN (lib include bin share) DO (
-	IF NOT EXIST "%PROJECT_DISTDIR%\%%d" (
-		MKDIR "%PROJECT_DISTDIR%\%%d"
-	)
+SETLOCAL
+
+:SETUP_VCPKG
+WHERE /Q vcpkg
+IF ERRORLEVEL 1 (
+	robocopy "%PROJECT_ROOT%\vendor\vcpkg" "%VCPKG_ROOT%" /E "/LOG:%PROJECT_TMPDIR%\robocopy.log"
+	PUSHD "%VCPKG_ROOT%"
+	.\bootstrap-vcpkg.bat
+	COPY /B "%VCPKG_ROOT%\vcpkg.exe" "%PROJECT_ROOT%\vcpkg.exe"
+	POPD
 )
+WHERE vcpkg
+IF ERRORLEVEL 1 GOTO :EOF
 
-:LOCATE_CPPUNIT
-@REM Best way to build upstream cppunit on NT is to use msbuild. Like our ngen setup script for it.
-CALL "%PROJECT_ROOT%\vendor\cppunit.ngen\cppunit-setup.cmd"
-@IF errorlevel 1 goto :EOF
+ECHO Installing dependencies with vcpkg.
 
-:LOCATE_BOOST
-@REM Boost is a monster.
-CALL "%PROJECT_ROOT%\vendor\boost.ngen\boost-setup.cmd"
-@IF errorlevel 1 goto :EOF
+vcpkg install --triplet %PROJECT_TARGET_TRIPLET% ^
+nlohmann-json ^
+zlib ^
+libarchive ^
+glfw3 ^
+cppunit ^
+boost-filesystem boost-optional boost-property-tree ^
+vulkan vulkan-hpp
 
-:LOCATE_JSON
-cmake -S "%PROJECT_ROOT%\vendor\json" -B "%PROJECT_TMPDIR%\json" -G Ninja "-DCMAKE_INSTALL_PREFIX=%PROJECT_DISTDIR%" -DCMAKE_BUILD_TYPE=Release
-@IF errorlevel 1 goto :EOF
-ninja -C "%PROJECT_TMPDIR%\json" install
-@IF errorlevel 1 goto :EOF
-
-:LOCATE_GLFW
-cmake -S "%PROJECT_ROOT%\vendor\glfw" -B "%PROJECT_TMPDIR%\glfw" -G Ninja "-DCMAKE_INSTALL_PREFIX=%PROJECT_DISTDIR%" -DCMAKE_BUILD_TYPE=Release
-@IF errorlevel 1 goto :EOF
-ninja -C "%PROJECT_TMPDIR%\glfw" install
-@IF errorlevel 1 goto :EOF
-
-:LOCATE_ZLIB
-@REM Can't build zlib from CMakeLists.txt, and also find_package() it without pain.
-cmake -S "%PROJECT_ROOT%\vendor\zlib" -B "%PROJECT_TMPDIR%\zlib" -G Ninja "-DCMAKE_INSTALL_PREFIX=%PROJECT_DISTDIR%" -DCMAKE_BUILD_TYPE=Release
-@IF errorlevel 1 goto :EOF
-ninja -C "%PROJECT_TMPDIR%\zlib" install
-@IF errorlevel 1 goto :EOF
-@REM clean up
-PUSHD vendor\zlib
-@IF errorlevel 1 goto :EOF
-git reset --hard HEAD
-git clean -f
-POPD
-
-:LOCATE_LIBARCHIVE
-cmake -S "%PROJECT_ROOT%\vendor\libarchive" -B "%PROJECT_TMPDIR%\libarchive" -G Ninja "-DCMAKE_INSTALL_PREFIX=%PROJECT_DISTDIR%" -DCMAKE_BUILD_TYPE=Release
-@IF errorlevel 1 goto :EOF
-ninja -C "%PROJECT_TMPDIR%\libarchive" install
-@IF errorlevel 1 goto :EOF
+IF ERRORLEVEL 1 GOTO :EOF
 
 
-ECHO Set your SXE_SDK=%PROJECT_DISTDIR% or get smarter.
+ENDLOCAL
+
 
