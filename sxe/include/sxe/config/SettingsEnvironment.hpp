2ff5a8e459ce58406513717e846c762753599de3
Terry M. Poulin <BigBoss1964@gmail.com>
2020-05-09 14:56:08 -0400

    SettingsEnvironment: a Settings impl' for env variables.
    This lets environment variables be used with Settings, with only a little
    pain and suffering. The main gap is that there's really no portable
    way to list all the environment variables.
    General setting environment variables is already system specific 8-).
    
    Or more apply the further from POSIX we get, the less I want to
    screw around with environ and relations.

---
+++ sxe/include/sxe/config/SettingsEnvironment.hpp
@@ -0,0 +1,78 @@
+#ifndef SXE_CONFIG_SETTINGSENVIRONMENT__HPP
+#define SXE_CONFIG_SETTINGSENVIRONMENT__HPP
+/*-
+ * Copyright (c) 2020-current, Terry Mathew Poulin <BigBoss1964@gmail.com>
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *	1. The origin of this software must not be misrepresented; you must
+ *	   not claim that you wrote the original software. If you use this
+ *	   software in a product, an acknowledgment in the product
+ *	   documentation would be appreciated but is not required.
+ *
+ *	2. Altered source versions must be plainly marked as such, and must
+ *	   not be misrepresented as being the original software.
+ *
+ *	3. This notice may not be removed or altered from any source
+ *	   distribution.
+ */
+
+#include <sxe/config/Settings.hpp>
+
+namespace sxe
+{
+    namespace config
+    {
+
+        /** Settings backed by $EnvironmentVariables.
+         *
+         * Gets use std::getenv().
+         * 
+         * Sets use setenv(), or similar platform specific functionality.
+         * 
+         * Depending on your target platform: some functionality may be ommitted.
+         * For example UWP applications do not have environment variables.
+         */
+        class SXE_PUBLIC SettingsEnvironment
+            : public Settings
+        {
+          public:
+            SettingsEnvironment();
+
+            virtual ~SettingsEnvironment() = default;
+
+            KeyList keys() const override;
+            bool contains(const string_type& key) const override;
+
+            string_type getString(const string_type& key) const override;
+
+            SettingsEnvironment& setString(const string_type& key, const string_type& value) override;
+
+            void clear() override;
+
+            bool save() override;
+
+          protected:
+          private:
+            static const string_type TAG;
+
+            /** Used to implement keys().
+             * 
+             * Because messing with environ is better avoided than ported, and unit tests assume default constructed Settings are empty...
+             */
+            std::set<string_type> mKeys;
+
+            static string_type get_env(const string_type& var);
+            static bool set_env(const string_type& var, const string_type& value);
+        };
+
+    } // namespace config
+} // namespace sxe
+
+#endif // SXE_CONFIG_SETTINGSENVIRONMENT__HPP
---
+++ sxe/src/config/SettingsEnvironment.cpp
@@ -0,0 +1,173 @@
+/*-
+ * Copyright (c) 2020-current, Terry Mathew Poulin <BigBoss1964@gmail.com>
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *	1. The origin of this software must not be misrepresented; you must
+ *	   not claim that you wrote the original software. If you use this
+ *	   software in a product, an acknowledgment in the product
+ *	   documentation would be appreciated but is not required.
+ *
+ *	2. Altered source versions must be plainly marked as such, and must
+ *	   not be misrepresented as being the original software.
+ *
+ *	3. This notice may not be removed or altered from any source
+ *	   distribution.
+ */
+
+#include <sxe/config/SettingsEnvironment.hpp>
+#include <sxe/logging.hpp>
+
+/*
+ * Not all platforms do this the same way.
+ * 
+ * std::getenv() is a classic C function that is pretty universal.
+ * POSIX systems use setenv(name, value, 1)
+ * WIN32 desktop uses _putenv_s(), etc.
+ * 
+ * Some platforms like UWP apps do not have environment variables.
+ */
+
+namespace sxe {  namespace config {
+
+const SettingsEnvironment::string_type SettingsEnvironment::TAG = "SettingsEnvironment";
+
+SettingsEnvironment::SettingsEnvironment()
+    : mKeys()
+{
+}
+
+
+SettingsEnvironment::KeyList SettingsEnvironment::keys() const
+{
+    KeyList r;
+
+    for (const string_type& k : mKeys) {
+        r.push_back(k);
+    }
+
+    return r;
+}
+
+
+bool SettingsEnvironment::contains(const string_type& key) const
+{
+    return !get_env(key).empty();
+}
+
+
+SettingsEnvironment::string_type SettingsEnvironment::getString(const string_type& key) const
+{
+    string_type value;
+
+    if (contains(key))
+        value = get_env(key);
+
+    Log::test(TAG, "getString(): key: " + key + " => value: " + value);
+    return value;
+}
+
+
+SettingsEnvironment& SettingsEnvironment::setString(const string_type& key, const string_type& value)
+{
+    Log::test(TAG, "setString(): key: " + key + " => value: " + value);
+
+    if (!set_env(key, value)) {
+        Log::w(TAG, "setString(): set_env() failed!");
+        return *this;
+    }
+
+    if (value.empty()) {
+        Log::test(TAG, "setString(): erase key");
+        mKeys.erase(key);
+    } else {
+        Log::test(TAG, "setString(): insert key");
+        mKeys.insert(key);
+    }
+
+    notifyListeners(key);
+    return *this;
+}
+
+
+void SettingsEnvironment::clear()
+{
+    Log::xtrace(TAG, "clear():");
+
+    for (const string_type& k : keys()) {
+        setString(k, "");
+    }
+}
+
+
+bool SettingsEnvironment::save()
+{
+    Log::d(TAG, "save(): no-op");
+    return true;
+}
+
+
+SettingsEnvironment::string_type SettingsEnvironment::get_env(const string_type& var)
+{
+#if defined(WINAPI_FAMILY_PARTITION) && !WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+
+    Log::w(TAG, "Windows only supports environment variables on the desktop API.");
+    return "";
+
+#else
+
+    char* p = std::getenv(var.c_str());
+    if (p == nullptr)
+        return "";
+
+    return string_type(p);
+
+#endif
+}
+
+
+bool SettingsEnvironment::set_env(const string_type& var, const string_type& value) 
+{
+
+#if defined(__unix__) || defined(_GNU_SOURCE)
+
+    int rc = setenv(var.c_str(), value.c_str(), 1);
+
+    if (rc != 0) {
+        Log::e(TAG, "set_env(): setenv() failed: " + strerror(errno));
+    }
+
+    return rc == 0;
+
+#elif defined(WINAPI_FAMILY_PARTITION) && !WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+
+    Log::w(TAG, "Windows only supports environment variables on the desktop API.");
+    return false;
+
+#elif defined(WINAPI_FAMILY_PARTITION) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+
+    errno_t rc = _putenv_s(var.c_str(), value.c_str());
+
+    if (rc != 0) {
+        string_type err(std::strerror(rc));
+        Log::e(TAG, "set_env() _putenv_s() failed: " + err);
+    }
+
+    return rc == 0;
+
+#else
+
+    Log::w(TAG, "No environment variables on this platform?");
+    return false;
+
+#endif
+}
+
+
+} }
