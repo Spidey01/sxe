f334f5c4c962460107cb103a2368fedbbf23766f
Terry M. Poulin <BigBoss1964@gmail.com>
2020-05-12 19:34:51 -0400

    bootstrap.cmd: better error checking.
    Abort if envsetup failed.
    
    Use CALL for the bootstrap of vcpkg.

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -11,6 +11,7 @@ git submodule update
 :CALL_ENVSETUP
 ECHO Setting up environment.
 IF NOT DEFINED PROJECT_ROOT ( CALL envsetup\envsetup.cmd )
+IF ERRORLEVEL 1 GOTO :EOF
 
 SETLOCAL
 
@@ -19,13 +20,19 @@ WHERE /Q vcpkg
 IF ERRORLEVEL 1 (
 	robocopy "%PROJECT_ROOT%\vendor\vcpkg" "%VCPKG_ROOT%" /E "/LOG:%PROJECT_TMPDIR%\robocopy.log"
 	PUSHD "%VCPKG_ROOT%"
-	.\bootstrap-vcpkg.bat
+	CALL .\bootstrap-vcpkg.bat
 	COPY /B "%VCPKG_ROOT%\vcpkg.exe" "%PROJECT_ROOT%\vcpkg.exe"
 	POPD
 )
 WHERE vcpkg
 IF ERRORLEVEL 1 GOTO :EOF
 
+IF EXIST vcpkg-export-*.zip (
+	ECHO vcpkg  importy time
+	GOTO :EOF
+)
+
+:INSTALL_DEPS
 ECHO Installing dependencies with vcpkg.
 
 vcpkg install --triplet %PROJECT_TARGET_TRIPLET% ^
