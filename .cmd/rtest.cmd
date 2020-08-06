444409da73f6fe2e3c9455286f5dcd53c527b586
Terry M. Poulin <BigBoss1964@gmail.com>
2020-08-05 21:20:14 -0400

    rdemo,itest: handle debug symbols on NT.
    vcpkg puts these in a debug tree. Such that debug\bin\*.dll and
    debug\lib\*.lib are the debug versions of bin\*.dll and lib\*.lib.
    
    Looks like using RelWithDebInfo wants to use debug versions of deps
    not just your own cmake project.

--- .cmd/rtest.cmd
+++ .cmd/rtest.cmd
@@ -10,6 +10,8 @@ SET XDG_CONFIG_DIRS=%PROJECT_DISTDIR%\etc\xdg
 SET XDG_DATA_HOME=%PROJECT_ROOT%\tmp\share
 SET XDG_CONFIG_HOME=%PROJECT_ROOT%\tmp\config
 SET XDG_CACHE_HOME=%PROJECT_ROOT%\tmp\cache
+@REM Using debug symbols will want these too.
+SET PATH=%Path%;%PROJECT_DISTDIR%\debug\bin
 
 @ECHO ON
 "%PROJECT_DISTDIR%\bin\sxe-test-runner" -o "%PROJECT_TMPDIR%\sxe-test-runner.log" %*
