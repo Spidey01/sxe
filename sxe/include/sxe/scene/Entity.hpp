b123caaa376224f45ce5a2f6051484178130819e
Terry M. Poulin <BigBoss1964@gmail.com>
2020-10-18 16:34:10 -0400

    Start on bringing over Entity.
    In 1.0 this was its own module, now it is part of the scene code.
    
    Originally the components available were:
    - InputFacet for taking input.
    - GraphicsFacet for being rendered.
    - RenderData for access to mesh, vbo, etc.
    
    For right now there is only a scene manager field.

---
+++ sxe/include/sxe/scene/Entity.hpp
@@ -0,0 +1,56 @@
+#ifndef SXE_SCENE_ENTITY__HPP
+#define SXE_SCENE_ENTITY__HPP
+/*-
+ * Copyright (c) 2014-current, Terry Mathew Poulin <BigBoss1964@gmail.com>
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *	1. The origin of this software must not be misrepresented; you must
+ *	   not claim that you wrote the original software. If you use this
+ *	   software in a product, an acknowledgment in the product
+ *	   documentation would be appreciated but is not required.
+ *
+ *	2. Altered source versions must be plainly marked as such, and must
+ *	   not be misrepresented as being the original software.
+ *
+ *	3. This notice may not be removed or altered from any source
+ *	   distribution.
+ */
+
+#include <sxe/common/stdtypedefs.hpp>
+
+namespace sxe { namespace scene {
+
+    class SceneManager;
+
+    /** Defines an entity that may be rendered in a scene.
+     *
+     */
+    class SXE_PUBLIC Entity
+        : public common::stdtypedefs<Entity>
+    {
+      public:
+        Entity();
+        ~Entity();
+
+        /** @returns the scene manager.
+         */
+        SceneManager* getSceneManager() const;
+
+        /** Sets the scene manager.
+         */
+        void setSceneManager(SceneManager* mgr);
+
+      private:
+        static const string_type TAG;
+        SceneManager* mSceneManager;
+    };
+} } 
+
+#endif // SXE_SCENE_ENTITY__HPP
---
+++ sxe/src/scene/Entity.cpp
@@ -0,0 +1,54 @@
+/*-
+ * Copyright (c) 2014-current, Terry Mathew Poulin <BigBoss1964@gmail.com>
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *	1. The origin of this software must not be misrepresented; you must
+ *	   not claim that you wrote the original software. If you use this
+ *	   software in a product, an acknowledgment in the product
+ *	   documentation would be appreciated but is not required.
+ *
+ *	2. Altered source versions must be plainly marked as such, and must
+ *	   not be misrepresented as being the original software.
+ *
+ *	3. This notice may not be removed or altered from any source
+ *	   distribution.
+ */
+
+#include "sxe/scene/Entity.hpp"
+
+#include <sxe/scene/SceneManager.hpp>
+#include <sxe/logging.hpp>
+
+namespace sxe { namespace scene {
+
+const Entity::string_type Entity::TAG = "GameEngine";
+
+Entity::Entity()
+    : mSceneManager(nullptr)
+{
+
+}
+
+Entity::~Entity()
+{
+
+}
+
+SceneManager* Entity::getSceneManager() const
+{
+    return mSceneManager;
+}
+
+void Entity::setSceneManager(SceneManager* mgr)
+{
+    mSceneManager = mgr;
+}
+
+} } 
