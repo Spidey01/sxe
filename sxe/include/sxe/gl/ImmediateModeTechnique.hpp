5f148f3dd9b78474bb2c792a3f2efa36d3e9144e
Terry M. Poulin <BigBoss1964@gmail.com>
2020-10-23 15:41:35 -0400

    A quick and dirty drawing technique for OpenGL 1.0.
    SxE 1.0 largely escewed the old style drawing API. Because my goal
    in writing SxE was to get used to the new programmable pipeline
    rather than the fixed function pipeline I was used to using.
    
    Two reasons I'm finally adding support for immediate mode:
    
      1.) It's really fast to screw with code.
      2.) I'd like to see if implementations finally say "NO" to 1.0.

---
+++ sxe/include/sxe/gl/ImmediateModeTechnique.hpp
@@ -0,0 +1,63 @@
+#ifndef SXE_GL_IMMEDIATEMODETECHNIQUE__HPP
+#define SXE_GL_IMMEDIATEMODETECHNIQUE__HPP
+/*-
+ * Copyright (c) 2020-current, Terry Mathew Poulin <BigBoss1964@gmail.com>
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *	1. The origin of this software must not be misrepresented; you must
+ *	   not claim that you wrote the original software. If you use this
+ *	   software in a product, an acknowledgment in the product
+ *	   documentation would be appreciated but is not required.
+ *
+ *	2. Altered source versions must be plainly marked as such, and must
+ *	   not be misrepresented as being the original software.
+ *
+ *	3. This notice may not be removed or altered from any source
+ *	   distribution.
+ */
+
+#include <sxe/api.hpp>
+#include <sxe/graphics/DrawingTechnique.hpp>
+
+namespace sxe
+{
+    namespace gl
+    {
+        /** Implements drawing technique with OpenGLES 1.0.
+         * 
+         * In virtually all cases you shouldn't use this technique. But it
+         * should work on anything that supports the early way of drawing via
+         * OpenGL.
+         */
+        class SXE_PUBLIC ImmediateModeTechnique
+            : public graphics::DrawingTechnique
+        {
+          public:
+            ImmediateModeTechnique();
+            virtual ~ImmediateModeTechnique();
+
+            /** Per frame startup.
+             */
+            void frameStarted() override;
+
+            /** Draw the graphics facet.
+             * 
+             * It is assumed that vertex information is in system memory, and
+             * suitable for use with GL_TRIANGLES.
+             */
+            void draw(graphics::GraphicsFacet& facet) override;
+
+          private:
+            static const string_type TAG;
+        };
+    } // namespace gl
+} // namespace sxe
+
+#endif // SXE_GL_IMMEDIATEMODETECHNIQUE__HPP
---
+++ sxe/src/gl/ImmediateModeTechnique.cpp
@@ -0,0 +1,74 @@
+/*-
+ * Copyright (c) 2020-current, Terry Mathew Poulin <BigBoss1964@gmail.com>
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *	1. The origin of this software must not be misrepresented; you must
+ *	   not claim that you wrote the original software. If you use this
+ *	   software in a product, an acknowledgment in the product
+ *	   documentation would be appreciated but is not required.
+ *
+ *	2. Altered source versions must be plainly marked as such, and must
+ *	   not be misrepresented as being the original software.
+ *
+ *	3. This notice may not be removed or altered from any source
+ *	   distribution.
+ */
+
+#include <sxe/gl/ImmediateModeTechnique.hpp>
+
+#include <glbinding/gl10/gl.h>
+#include <sxe/graphics/Vertex.hpp>
+#include <sxe/logging.hpp>
+
+using sxe::graphics::Vertex;
+using sxe::graphics::GraphicsFacet;
+
+namespace sxe
+{
+    namespace gl
+    {
+        const ImmediateModeTechnique::string_type ImmediateModeTechnique::TAG = "ImmediateModeTechnique";
+
+        ImmediateModeTechnique::ImmediateModeTechnique()
+            : DrawingTechnique(TAG, "OpenGL[ES] legacy immediate mode.")
+        {
+        }
+
+        ImmediateModeTechnique::~ImmediateModeTechnique()
+        {
+        }
+
+        void ImmediateModeTechnique::frameStarted()
+        {
+            DrawingTechnique::frameStarted();
+
+            gl10::glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            gl10::ClearBufferMask mask = gl10::ClearBufferMask::GL_COLOR_BUFFER_BIT | gl10::ClearBufferMask::GL_DEPTH_BUFFER_BIT | gl10::ClearBufferMask::GL_STENCIL_BUFFER_BIT;
+            gl10::glClear(mask);
+
+            gl10::glLoadIdentity();
+        }
+
+        void ImmediateModeTechnique::draw(GraphicsFacet& facet)
+        {
+            DrawingTechnique::draw(facet);
+
+            gl10::glBegin(gl10::GL_TRIANGLES);
+            {
+                for (const Vertex& vert : facet.verticesAsVector()) {
+                    gl10::glColor3f(vert.color.r, vert.color.g, vert.color.b);
+                    gl10::glVertex3f(vert.pos.x, vert.pos.y, vert.pos.y);
+                }
+            }
+            gl10::glEnd();
+        }
+
+    } // namespace gl
+} // namespace sxe
