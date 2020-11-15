d4c2e6a21daf94abf1a5eae3ea37413b82894d48
Terry M. Poulin <BigBoss1964@gmail.com>
2020-11-15 16:53:16 -0500

    rdemo: set runtime dir to project's tmp.

--- .cmd/rdemo.cmd
+++ .cmd/rdemo.cmd
@@ -15,6 +15,7 @@ SET XDG_CONFIG_DIRS=%PROJECT_DISTDIR%\etc\xdg
 SET XDG_DATA_HOME=%PROJECT_ROOT%\tmp\share
 SET XDG_CONFIG_HOME=%PROJECT_ROOT%\tmp\config
 SET XDG_CACHE_HOME=%PROJECT_ROOT%\tmp\cache
+SET XDG_RUNTIME_DIR=%PROJECT_ROOT%\tmp
 @REM Using debug symbols will want these too.
 SET PATH=%Path%;%PROJECT_DISTDIR%\debug\bin
 
